7fe62ec75b193eb0ce6a8cfd782094cba747916d
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-09-24 20:34:56 +0300

    Shopping cart added, some small fixes

--- Assets/JS/ShoppingCart.js
+++ Assets/JS/ShoppingCart.js
@@ -1,40 +1,25 @@
-var loginForm = document.getElementById('logIn');
-var login = document.getElementById('logInButton')
-var background = document.getElementById('blackBackground')
-login.addEventListener('click', function (event) {
-    loginForm.style.display = 'inline-block';
-    loginForm.style.position = 'fixed';
-    loginForm.style.top = '50%';
-    loginForm.style.left = '50%';
-    //300X270
-    loginForm.style.marginTop = '-150px';
-    loginForm.style.marginLeft = '-135px';
+var cart = document.getElementById('cart');
+var cartContainer = document.getElementById('CartContainer');
+var background = document.getElementById('blackBackground');
+
+cart.addEventListener('click', function(event){
+    cartContainer.style.display = 'inline-block';
     background.style.width = '100%';
     background.style.height = '100%';
     background.style.display = 'inline-block';
-    document.body.style.overflow = 'hidden';
-    
-
-    
-
-
-
-    console.log('raboti')
+    document.body.style.overflow = 'hidden'
 })
 document.addEventListener('keydown', function (event) {
     if (event.keyCode == 27) {
         document.getElementById('blackBackground').style.display = 'none';
-        loginForm.style.display = 'none';
+        cartContainer.style.display = 'none';
         document.body.style.overflow = 'scroll';
-
-
     }
 })
 window.addEventListener('click', function (event) {
     if ((event.target == document.getElementById('blackBackground') && event.target != login) || event.target == document.getElementById('closeButtonLogin')) {
         document.getElementById('blackBackground').style.display = 'none';
-        loginForm.style.display = 'none';
+        cartContainer.style.display = 'none';
         document.body.style.overflow = 'scroll';
-
     }
 })
