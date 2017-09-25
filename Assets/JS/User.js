e1f47d15271f8163f3b88f6ae31e02c96a884698
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-09-25 09:21:27 +0300

    Moved user to diffrent .js file

---
+++ Assets/JS/User.js
@@ -0,0 +1,23 @@
+function User(username, password, email) {
+    this.id = 1;
+    this.username = username;
+    var password = password;
+    this.email = email;
+    this.address = {
+        region: "",
+        city: '',
+        street: '',
+        number: '',
+        block: '',
+        entr: '',
+        floor: '',
+        apartment: ''
+    }
+
+    this.getPassword = function () {
+        return password;
+    }
+    this.setPassword = function (newPassword) {
+        password = newPassword;
+    }
+};
--- Assets/JS/userRegistration.js
+++ Assets/JS/userRegistration.js
@@ -1,29 +1,3 @@
-
-function User(username, password, email) {
-    this.id = 1;
-    this.username = username;
-    var password = password;
-    this.email = email;
-    this.address={
-        region:"",
-        city: '',
-        street: '',
-        number: '',
-        block: '',
-        entr: '',
-        floor: '',
-        apartment: ''
-    }
-
-    this.getPassword = function () {
-        return password;
-    }
-    this.setPassword = function (newPassword) {
-        password = newPassword;
-    }
-};
-
-
 function UsersDB() {
     this.users = [];
     this.id = 0;
