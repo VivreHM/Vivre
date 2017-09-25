4b2eb6dc16cafd0124eafe7a037f0244dc0db504
hcsubev <xc.photo.design@gmail.com>
2017-09-25 14:47:14 +0300

    Small changes to the log in animations

--- Assets/JS/userRegistration.js
+++ Assets/JS/userRegistration.js
@@ -1,20 +1,23 @@
 
 document.getElementById("regButton").addEventListener("click", function (event) {
     event.preventDefault();
+
     if (!userDB.users.some(a => a.username == document.getElementById("username").value)) {
         var usern = document.getElementById("username").value;
     } else {
         document.getElementById("username").value = "";
-        document.getElementById("username").style.backgroundColor = '#AA3939';
+        document.getElementById("username").style.backgroundColor = '#F3D967';
+        document.getElementById("regButton").classList.add("shake1");
         document.getElementById("username").placeholder = 'Това потребителско име вече съществува.'
     }
     if (document.getElementById("passwordFirst").value === document.getElementById("passwordSecond").value) {
         var pass = document.getElementById("passwordFirst").value;
     } else {
         ///alert("Въведените пароли са различни.")
-        document.getElementById('passwordFirst').style.backgroundColor = '#AA3939';
-        document.getElementById("passwordSecond").style.backgroundColor = '#AA3939';
-        document.getElementById("passwordSecond").placeholder = 'Паролата трябва да е същата като първата';        
+        document.getElementById('passwordFirst').style.backgroundColor = '#F3D967';
+        document.getElementById("passwordSecond").style.backgroundColor = '#F3D967';
+        document.getElementById("passwordSecond").placeholder = 'Паролата трябва да е същата като първата';
+        document.getElementById("regButton").classList.add("shake1");        
         return;
     }
     var email = document.getElementById("email").value;
@@ -27,6 +30,7 @@ document.getElementById("regButton").addEventListener("click", function (event) 
         document.getElementById('blackBackground').style.display = 'none';
         document.body.style.overflow = 'scroll'
     }
+    setTimeout(function(){ document.getElementById("regButton").classList.remove("shake1"); }, 2000);
     console.log(userDB.users);
 });
 
