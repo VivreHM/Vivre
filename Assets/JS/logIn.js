23af6bf7a6b6dcdb23540b874a399f87234fcf09
hcsubev <xc.photo.design@gmail.com>
2017-09-25 23:31:12 +0300

    Added stronger user validation

--- Assets/JS/logIn.js
+++ Assets/JS/logIn.js
@@ -5,22 +5,31 @@ document.getElementById("logInFormButton").addEventListener("click",function(eve
     var userName = document.getElementById("logInUsername").value;
     var user = userDB.users.find(a=>a.username==userName)
     if(user!=undefined){
+        document.getElementById("logInUsername").style.backgroundColor = 'white';
         var pass = document.getElementById("logInPassword").value;
-        if(user.getPassword()=== pass){
-            alert("Влезли сте!")
+        if(user.getPassword() == pass && pass){
             document.getElementById("signButtons").style.display="none";
             document.getElementById("userButton").innerHTML = user.username;
             document.getElementById("userInfo").style.display="inline-block";
             document.getElementById("potrebitel").textContent = user.username;
             document.getElementById("userEmail").textContent = user.email;
             signedUser = user;
+            document.getElementById('logIn').style.display = 'none';
+            document.getElementById('blackBackground').style.display = 'none';
+            document.body.style.overflow = 'scroll'
+            document.getElementById("logInPassword").style.backgroundColor = 'white';
+        }else{
+            document.getElementById("logInPassword").value = "";
+            document.getElementById("logInPassword").style.backgroundColor = '#F3D967';
+            document.getElementById("logInFormButton").classList.add("shake1");
         }
+    }else{
+        document.getElementById("logInUsername").value = "";
+        document.getElementById("logInUsername").style.backgroundColor = '#F3D967';
+        document.getElementById("logInPassword").style.backgroundColor = '#F3D967';
+        document.getElementById("logInFormButton").classList.add("shake1");
     }
-    if (user) {
-        document.getElementById('logIn').style.display = 'none';
-        document.getElementById('blackBackground').style.display = 'none';
-        document.body.style.overflow = 'scroll'
-    }
+    setTimeout(function () { document.getElementById("logInFormButton").classList.remove("shake1"); }, 2000);
 })
 document.getElementById('registerInLogIn').addEventListener('click',function (event) {
     event.preventDefault();
@@ -28,3 +37,4 @@ document.getElementById('registerInLogIn').addEventListener('click',function (ev
     document.getElementById('register').style.pposition = 'fixed';
     document.getElementById('register').style.display = 'inline-block';
 })
+
--- Assets/JS/userRegistration.js
+++ Assets/JS/userRegistration.js
@@ -2,25 +2,32 @@
 document.getElementById("regButton").addEventListener("click", function (event) {
     event.preventDefault();
 
-    if (!userDB.users.some(a => a.username == document.getElementById("username").value)) {
+    if (!userDB.users.some(a => a.username == document.getElementById("username").value.trim()) && 
+    document.getElementById("username").value) {
+        document.getElementById("username").style.backgroundColor = 'white';
         var usern = document.getElementById("username").value;
     } else {
         document.getElementById("username").value = "";
         document.getElementById("username").style.backgroundColor = '#F3D967';
-        document.getElementById("regButton").classList.add("shake1");
-        document.getElementById("username").placeholder = 'Това потребителско име вече съществува.'
     }
-    if (document.getElementById("passwordFirst").value === document.getElementById("passwordSecond").value) {
+    if (document.getElementById("passwordFirst").value === document.getElementById("passwordSecond").value &&
+    document.getElementById("passwordFirst").value) {
+        document.getElementById('passwordFirst').style.backgroundColor = 'white';
+        document.getElementById("passwordSecond").style.backgroundColor = 'white';
         var pass = document.getElementById("passwordFirst").value;
     } else {
         ///alert("Въведените пароли са различни.")
         document.getElementById('passwordFirst').style.backgroundColor = '#F3D967';
         document.getElementById("passwordSecond").style.backgroundColor = '#F3D967';
-        document.getElementById("passwordSecond").placeholder = 'Паролата трябва да е същата като първата';
-        document.getElementById("regButton").classList.add("shake1");        
-        return;
+        //document.getElementById("passwordSecond").placeholder = 'Паролата трябва да е същата като първата';
     }
-    var email = document.getElementById("email").value;
+    if (document.getElementById("email").value && 
+        !userDB.users.some(a => a.email == document.getElementById("email").value.trim())) {
+        var email = document.getElementById("email").value;
+        document.getElementById('email').style.backgroundColor = 'white';
+    }else{
+        document.getElementById('email').style.backgroundColor = '#F3D967';
+    }
     if (usern && pass && email) {
         var user = new User(usern, pass, email);
         userDB.addUser(user);
@@ -29,8 +36,10 @@ document.getElementById("regButton").addEventListener("click", function (event) 
         document.getElementById('register').style.display = 'none';
         document.getElementById('blackBackground').style.display = 'none';
         document.body.style.overflow = 'scroll'
+    } else {
+        document.getElementById("regButton").classList.add("shake1");
     }
-    setTimeout(function(){ document.getElementById("regButton").classList.remove("shake1"); }, 2000);
+    setTimeout(function () { document.getElementById("regButton").classList.remove("shake1"); }, 2000);
     console.log(userDB.users);
 });
 
