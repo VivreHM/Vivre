50e645caa7d6a7aba17ef514a689524f40fa6c75
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-09-25 11:09:12 +0300

    look comment from previous commit

--- Assets/JS/SubMenu.js
+++ Assets/JS/SubMenu.js
@@ -2,8 +2,15 @@ var navMenu = document.querySelectorAll('.navMenu');
 var subMenu = document.querySelectorAll('.subMenu');
 
 var idx = 0;
-var currentMenu = null;
+//var currentMenu = null;
 navMenu.forEach(function (element, index) {
+    element.addEventListener('mouseleave', function(event){
+        subMenu[index].style.display ='none';
+    })
+    subMenu[index].addEventListener('mouseover', function(event){
+        subMenu[index].style.display = 'inline-block';
+        
+    })
     element.addEventListener('mouseover', function (event) {
         event.preventDefault();
         subMenu[idx].style.display = 'none';
--- Assets/JS/saveAddress.js
+++ Assets/JS/saveAddress.js
@@ -1,45 +1,79 @@
-document.getElementById("save").addEventListener('click',function(){
-    signedUser.address.region = document.getElementById("region").value;
-    var newRegion = document.createElement("span")
-    newRegion.textContent = signedUser.address.region;
-    document.getElementById("region").parentNode.replaceChild(newRegion,document.getElementById("region"))
+var inputAdress = document.querySelectorAll('.inputAdress');
 
-    signedUser.address.city = document.getElementById("city").value;
-    var newCity= document.createElement("span");
-    newCity.textContent = signedUser.address.city;
-    document.getElementById("city").parentNode.replaceChild(newCity,document.getElementById("city"));
-    
-    signedUser.address.street = document.getElementById("street").value;
-    var newStr= document.createElement("span");
-    newStr.textContent = signedUser.address.street;
-    document.getElementById("street").parentNode.replaceChild(newStr, document.getElementById("street"));
-    
-    signedUser.address.number = document.getElementById("number").value;
-    var newNum= document.createElement("span");
-    newNum.textContent = signedUser.address.number;
-    document.getElementById("number").parentNode.replaceChild(newNum, document.getElementById("number"));
-    
-    signedUser.address.block = document.getElementById("block").value;
-    var newBlock= document.createElement("span");
-    newBlock.textContent = signedUser.address.block;
-    document.getElementById("block").parentNode.replaceChild(newBlock,document.getElementById("block"));
-    
-    signedUser.address.entr = document.getElementById("еntr").value;
-    var newEntr= document.createElement("span")
-    newEntr.textContent = signedUser.address.entr;
-    document.getElementById("еntr").parentNode.replaceChild(newEntr,document.getElementById("еntr"))
-    
-    signedUser.address.floor = document.getElementById("floor").value;
-    var newFloor= document.createElement("span")
-    newFloor.textContent = signedUser.address.floor;
-    document.getElementById("floor").parentNode.replaceChild(newFloor,document.getElementById("floor"))
-    
-    signedUser.address.apartment = document.getElementById("apartment").value;
-    var newApart= document.createElement("span")
-    newApart.textContent = signedUser.address.apartment;
-    document.getElementById("apartment").parentNode.replaceChild(newApart,document.getElementById("apartment"))
-    document.getElementById("inputs").style.lineHeight="220%";
-    document.getElementById("inputs").style.fontFamily="'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif%";
-    document.getElementById("inputs").style.paddingTop="9px";
-    document.getElementById("save").style.display="none"
+document.getElementById("save").addEventListener('click', function () {
+    var isFilled = true;
+    console.log(inputAdress)
+    Array.prototype.forEach.call(inputAdress, function (element) {
+        console.log(element);
+        if (element.value == '') {
+            element.style.backgroundColor = '#F6E79D';
+            isFilled = false;
+        }
+    })
+
+    if (isFilled) {
+        var keys = [];
+        for (var userAdress in signedUser.adress) {
+            keys.push(userAdress);
+        }
+        Array.prototype.forEach.call(inputAdress, function (element, index) {
+            signedUser.address[keys[index]] = element.value;
+            var newAdress = document.createElement('span');
+            newAdress.textContent = element.value;
+            element.parentNode.replaceChild(newAdress, element)
+        })
+
+        // signedUser.address.region = document.getElementById("region").value;
+        // var newRegion = document.createElement("span")
+        // newRegion.textContent = signedUser.address.region;
+        // document.getElementById("region").parentNode.replaceChild(newRegion, document.getElementById("region"))
+
+        // signedUser.address.city = document.getElementById("city").value;
+        // var newCity = document.createElement("span");
+        // newCity.textContent = signedUser.address.city;
+        // document.getElementById("city").parentNode.replaceChild(newCity, document.getElementById("city"));
+
+        // signedUser.address.street = document.getElementById("street").value;
+        // var newStr = document.createElement("span");
+        // newStr.textContent = signedUser.address.street;
+        // document.getElementById("street").parentNode.replaceChild(newStr, document.getElementById("street"));
+
+        // signedUser.address.number = document.getElementById("number").value;
+        // var newNum = document.createElement("span");
+        // newNum.textContent = signedUser.address.number;
+        // document.getElementById("number").parentNode.replaceChild(newNum, document.getElementById("number"));
+
+        // signedUser.address.block = document.getElementById("block").value;
+        // var newBlock = document.createElement("span");
+        // newBlock.textContent = signedUser.address.block;
+        // document.getElementById("block").parentNode.replaceChild(newBlock, document.getElementById("block"));
+
+        // signedUser.address.entr = document.getElementById("еntr").value;
+        // var newEntr = document.createElement("span")
+        // newEntr.textContent = signedUser.address.entr;
+        // document.getElementById("еntr").parentNode.replaceChild(newEntr, document.getElementById("еntr"))
+
+        // signedUser.address.floor = document.getElementById("floor").value;
+        // var newFloor = document.createElement("span")
+        // newFloor.textContent = signedUser.address.floor;
+        // document.getElementById("floor").parentNode.replaceChild(newFloor, document.getElementById("floor"))
+
+        // signedUser.address.apartment = document.getElementById("apartment").value;
+        // var newApart = document.createElement("span")
+        // newApart.textContent = signedUser.address.apartment;
+        // document.getElementById("apartment").parentNode.replaceChild(newApart, document.getElementById("apartment"))
+        document.getElementById("inputs").style.lineHeight = "220%";
+        document.getElementById("inputs").style.fontFamily = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif%";
+        document.getElementById("inputs").style.paddingTop = "9px";
+        document.getElementById("save").style.display = "none"
+    }
+})
+
+Array.prototype.forEach.call(inputAdress, function (element) {
+    console.log(element);
+    element.addEventListener('change', function (event) {
+        if (element.value !== '') {
+            element.style.backgroundColor = 'white';
+        }
+    })
 })
