cf422489c2f1d38d88563f2544136c9229dc1b06
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-09-26 23:51:20 +0300

    added mainPage div to hide everything in main page and fixed in userInfo.js file

--- Assets/JS/userInfo.js
+++ Assets/JS/userInfo.js
@@ -2,6 +2,6 @@ document.getElementById("userButton").addEventListener("click", function(){
     document.getElementById("userPage").style.display="inline-block";
     document.getElementById("searchBar").style.display="none";
     document.querySelector("nav").style.display="none";
-    document.getElementById("slideshow").style.display="none";
+    document.getElementById('mainPage').style.display = 'none'
     document.getElementById("userInfo").style.display="inline block";
 })
