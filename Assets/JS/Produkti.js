12f3094fd4ab2d7ca1bd1a8d8690457bce282cee
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-10-02 09:11:50 +0300

    sort products by price

--- Assets/JS/Produkti.js
+++ Assets/JS/Produkti.js
@@ -12,7 +12,7 @@ var productsDB = (function () {
         if (localStorage.getItem('products') != null) {
             this._products = JSON.parse(localStorage.getItem('products'))
         } else {
-            this._products = [new Product('Стол', 'Assets/Images/Products/product1.jpg', '149.90лв', 'стол', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product2.png', '149.90лв', 'стол', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product1.png', '149.90лв', 'стол', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product1.jpg', '149.90лв', 'стол', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product1.jpg', '149.90лв', 'стол', 'дъб и дърво')];
+            this._products = [new Product('Стол', 'Assets/Images/Products/product1.jpg', '149.90лв', 'Мебели', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product2.png', '149.90лв', 'Кухня', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product1.png', '49.90лв', 'стол', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product1.jpg', '149.90лв', 'стол', 'дъб и дърво'),new Product('Стол', 'Assets/Images/Products/product1.jpg', '149.90лв', 'стол', 'дъб и дърво')];
             localStorage.setItem('products', JSON.stringify(this._products));
         }
         this.products = [];
@@ -21,6 +21,20 @@ var productsDB = (function () {
     ProductDB.prototype.addProduct = function (product) {
         this._products.push(product);
         localStorage.setItem('products', JSON.stringify(this._products));
+    }
+    ProductDB.prototype.filterByPriceLowestFirst = function(products){
+        products.sort(function(element1, element2){
+            return parseInt(element1.price) - parseInt(element2.price);
+        })
+    }
+    ProductDB.prototype.filterByPriceHighestFirst = function(products){
+        products.sort(function(element1, element2){
+            return parseInt(element2.price) - parseInt(element1.price);
+        })
+    }
+    ProductDB.prototype.filterByType = function(products, type){
+        console.log('vlazoh tuk')
+       return products.filter(x => x.type == type)
     }
     var productsDB = new ProductDB;
     return productsDB;
--- Assets/JS/ProduktiController.js
+++ Assets/JS/ProduktiController.js
@@ -1,5 +1,31 @@
-var productTemplate = document.getElementById('entry-template').innerHTML
-var template = Handlebars.compile(productTemplate);
-var readyHTML = template(productsDB);
-console.log(readyHTML);
-document.getElementById('content').innerHTML = readyHTML;
+function changeProductsDisplay() {
+    var productTemplate = document.getElementById('entry-template').innerHTML
+    var template = Handlebars.compile(productTemplate);
+    var readyHTML = template(productsDB);
+    document.getElementById('content').innerHTML = readyHTML;
+}
+changeProductsDisplay();
+var filterByPrice = document.getElementById('sortField');
+var filterByType = document.querySelectorAll('.filters');
+var checkboxes = document.querySelectorAll('.checkboxes')
+filterByPrice.addEventListener('change', function () {
+    if (filterByPrice.value == '2') {
+        productsDB.filterByPriceLowestFirst(productsDB._products)
+        changeProductsDisplay();      
+    }
+    if (filterByPrice.value == '1') {
+        productsDB.filterByPriceHighestFirst(productsDB._products)
+        changeProductsDisplay();
+    }
+    if (filterByPrice.value == '3') {
+        productsDB.filterByPriceLowestFirst(productsDB._products)
+        changeProductsDisplay();       
+    }
+})
+checkboxes.forEach(function(element){
+    element.addEventListener('click', function(){
+           productsDB= productsDB.filterByType(productsDB._products, element.nextElementSibling.value);
+            changeProductsDisplay();
+        
+    })
+})
