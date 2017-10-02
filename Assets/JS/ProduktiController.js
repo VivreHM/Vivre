ea368a4ad667a626e066ffcd0351bdeba1ac86bc
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-10-02 10:01:09 +0300

    card animation in filter fixed

--- Assets/JS/ProduktiController.js
+++ Assets/JS/ProduktiController.js
@@ -1,8 +1,54 @@
+function cardAnimation() {
+    var imageHolder = document.querySelectorAll(".imageHolder");
+    var productInfo = document.querySelectorAll(".productInformation");
+    imageHolder.forEach(function (element, index) {
+        element.addEventListener('click', function () {
+            setTimeout(function () {
+                element.style.top = "-26px";
+                element.style.transform = "scale(0.85,0.85)";
+                element.style.zIndex = "5";
+
+                productInfo[index].style.transform = 'scale(1,1)';
+                productInfo[index].style.top = '0px';
+                productInfo[index].style.zIndex = "15";
+
+                element.classList.remove('triggerAnimation');
+                productInfo[index].classList.remove('triggerAnimationBack');
+            }, 302)
+            element.classList.remove('triggerAnimationBack');
+            productInfo[index].classList.remove('triggerAnimation');
+            element.classList.add('triggerAnimation');
+            productInfo[index].classList.add('triggerAnimationBack');
+        })
+        productInfo[index].addEventListener('click', function (event) {
+            if (event.target != document.querySelectorAll('.wantedQuantity')[index]
+                && event.target != document.querySelectorAll('.addProductToCart')[index]
+                && event.target != document.querySelectorAll('.moreInformation')[index]) {
+                setTimeout(function () {
+                    productInfo[index].style.top = "-26px";
+                    productInfo[index].style.transform = "scale(0.85,0.85)";
+                    productInfo[index].style.zIndex = "5";
+
+                    element.style.transform = 'scale(1,1)';
+                    element.style.top = '0px';
+                    element.style.zIndex = "15";
+
+                    element.classList.remove('triggerAnimationBack');
+                    productInfo[index].classList.remove('triggerAnimation');
+                }, 302)
+                element.classList.add('triggerAnimationBack');
+                productInfo[index].classList.add('triggerAnimation');
+            }
+        })
+    })
+    return cardAnimation;
+};
 function changeProductsDisplay() {
     var productTemplate = document.getElementById('entry-template').innerHTML
     var template = Handlebars.compile(productTemplate);
     var readyHTML = template(productsDB);
     document.getElementById('content').innerHTML = readyHTML;
+    cardAnimation();
 }
 changeProductsDisplay();
 var filterByPrice = document.getElementById('sortField');
@@ -11,7 +57,7 @@ var checkboxes = document.querySelectorAll('.checkboxes')
 filterByPrice.addEventListener('change', function () {
     if (filterByPrice.value == '2') {
         productsDB.filterByPriceLowestFirst(productsDB._products)
-        changeProductsDisplay();      
+        changeProductsDisplay();
     }
     if (filterByPrice.value == '1') {
         productsDB.filterByPriceHighestFirst(productsDB._products)
@@ -19,13 +65,13 @@ filterByPrice.addEventListener('change', function () {
     }
     if (filterByPrice.value == '3') {
         productsDB.filterByPriceLowestFirst(productsDB._products)
-        changeProductsDisplay();       
+        changeProductsDisplay();
     }
 })
-checkboxes.forEach(function(element){
-    element.addEventListener('click', function(){
-           productsDB= productsDB.filterByType(productsDB._products, element.nextElementSibling.value);
-            changeProductsDisplay();
-        
+checkboxes.forEach(function (element) {
+    element.addEventListener('click', function () {
+        productsDB = productsDB.filterByType(productsDB._products, element.nextElementSibling.value);
+        changeProductsDisplay();
+
     })
 })
--- Assets/JS/productClickEffect.js
+++ Assets/JS/productClickEffect.js
@@ -1,43 +1,47 @@
+// var cardAnimation = function() {
+//     var imageHolder = document.querySelectorAll(".imageHolder");
+//     var productInfo = document.querySelectorAll(".productInformation");
+//     imageHolder.forEach(function (element, index) {
+//         element.addEventListener('click', function () {
+//             setTimeout(function () {
+//                 element.style.top = "-26px";
+//                 element.style.transform = "scale(0.85,0.85)";
+//                 element.style.zIndex = "5";
 
-var imageHolder = document.querySelectorAll(".imageHolder");
-var productInfo = document.querySelectorAll(".productInformation");
-imageHolder.forEach(function (element, index) {
-    element.addEventListener('click', function () {
-        setTimeout(function () {
-            element.style.top = "-26px";
-            element.style.transform = "scale(0.85,0.85)";
-            element.style.zIndex = "5";
+//                 productInfo[index].style.transform = 'scale(1,1)';
+//                 productInfo[index].style.top = '0px';
+//                 productInfo[index].style.zIndex = "15";
 
-            productInfo[index].style.transform = 'scale(1,1)';
-            productInfo[index].style.top = '0px';
-            productInfo[index].style.zIndex = "15";
+//                 element.classList.remove('triggerAnimation');
+//                 productInfo[index].classList.remove('triggerAnimationBack');
+//             }, 302)
+//             element.classList.remove('triggerAnimationBack');
+//             productInfo[index].classList.remove('triggerAnimation');
+//             element.classList.add('triggerAnimation');
+//             productInfo[index].classList.add('triggerAnimationBack');
+//         })
+//         productInfo[index].addEventListener('click', function (event) {
+//             if (event.target != document.querySelectorAll('.wantedQuantity')[index]
+//                 && event.target != document.querySelectorAll('.addProductToCart')[index]
+//                 && event.target != document.querySelectorAll('.moreInformation')[index]) {
+//                 setTimeout(function () {
+//                     productInfo[index].style.top = "-26px";
+//                     productInfo[index].style.transform = "scale(0.85,0.85)";
+//                     productInfo[index].style.zIndex = "5";
 
-            element.classList.remove('triggerAnimation');
-            productInfo[index].classList.remove('triggerAnimationBack');
-        }, 302)
-        element.classList.remove('triggerAnimationBack');
-        productInfo[index].classList.remove('triggerAnimation');
-        element.classList.add('triggerAnimation');
-        productInfo[index].classList.add('triggerAnimationBack');
-    })
-    productInfo[index].addEventListener('click', function (event) {
-        if (event.target != document.querySelectorAll('.wantedQuantity')[index]
-            && event.target != document.querySelectorAll('.addProductToCart')[index]
-            && event.target != document.querySelectorAll('.moreInformation')[index]) {
-            setTimeout(function () {
-                productInfo[index].style.top = "-26px";
-                productInfo[index].style.transform = "scale(0.85,0.85)";
-                productInfo[index].style.zIndex = "5";
+//                     element.style.transform = 'scale(1,1)';
+//                     element.style.top = '0px';
+//                     element.style.zIndex = "15";
 
-                element.style.transform = 'scale(1,1)';
-                element.style.top = '0px';
-                element.style.zIndex = "15";
+//                     element.classList.remove('triggerAnimationBack');
+//                     productInfo[index].classList.remove('triggerAnimation');
+//                 }, 302)
+//                 element.classList.add('triggerAnimationBack');
+//                 productInfo[index].classList.add('triggerAnimation');
+//             }
+//         })
+//     })
+//     return cardAnimation;
+// };
 
-                element.classList.remove('triggerAnimationBack');
-                productInfo[index].classList.remove('triggerAnimation');
-            }, 302)
-            element.classList.add('triggerAnimationBack');
-            productInfo[index].classList.add('triggerAnimation');
-        }
-    })
-})
+cardAnimation();
