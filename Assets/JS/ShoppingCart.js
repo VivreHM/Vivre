ce47e4c0fbde2249d8abdcd3ca174b4b6c7ccabc
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-10-08 16:01:29 +0300

    remove product from cart

--- Assets/JS/ShoppingCart.js
+++ Assets/JS/ShoppingCart.js
@@ -3,34 +3,79 @@ var cartContainer = document.getElementById('CartContainer');
 var background = document.getElementById('blackBackground');
 var addButtonsOnProduct = document.querySelectorAll('.addProductToCart');
 var cartTemplate = document.getElementById('cart-template').innerHTML;
+var addedProducts = [];
 var totalPrice = 0;
-addButtonsOnProduct.forEach(function(button){
-    button.addEventListener('click', function(){
+addButtonsOnProduct.forEach(function (button) {
+    button.addEventListener('click', function () {
         var id = button.className.slice(-1);
         var product = productsDB.getElementById(id);
-        console.log(product)
-        addToCart(product[0]); 
-        totalPrice = (parseFloat(product[0].price) + parseFloat(totalPrice)).toFixed(2);
-        console.log(product[0].price + 'cena na produkta')
-        console.log(totalPrice)
-        document.getElementById('totalPrice').innerHTML = parseFloat(totalPrice).toFixed(2);
-        
+        if (addedProducts.filter(x => x.id == product.id).length <= 0) {
+            var wantedQuantity = 'wantedQuantity' + product.id;
+            var wantedQuantityInProduct = 'wantedQuantityInProduct' + product.id;
+            var value = parseInt(document.getElementById(wantedQuantityInProduct).value);
+            addedProducts.push(product);
+            addToCart(product);
+            document.getElementById(wantedQuantity).value = value;
+            totalPrice = ((parseFloat(product.price) * value) + parseFloat(totalPrice)).toFixed(2);
+            document.getElementById('totalPrice').innerHTML = parseFloat(totalPrice).toFixed(2);
+            removeFromOrder();
+            changePrice();
+        } else {
+            var wantedQuantity = 'wantedQuantity' + product.id;
+            var wantedQuantityInProduct = 'wantedQuantityInProduct' + product.id;
+            var value = parseInt(document.getElementById(wantedQuantity).value);
+            value += parseInt(document.getElementById(wantedQuantityInProduct).value);
+            document.getElementById(wantedQuantity).value = value;
+            totalPrice = ((parseFloat(product.price) * value) + parseFloat(totalPrice)).toFixed(2);
+            document.getElementById('totalPrice').innerHTML = parseFloat(totalPrice).toFixed(2);
+            changePrice();
+        }
     })
 })
+var changePrice = function () {
+    document.querySelectorAll('.wantedQuantityInCart').forEach(function (element, index) {
+        element.addEventListener('blur', function () {
+            var wantedQuantity = 'wantedQuantity' + product.id;
+            var value = parseInt(document.getElementById(wantedQuantity).value);
+            totalPrice = ((parseFloat(product.price) * value) + parseFloat(totalPrice)).toFixed(2);
+            document.getElementById('totalPrice').innerHTML = parseFloat(totalPrice).toFixed(2);
+            
+        })
+    })
+}
 var addToCart = function (product) {
     var template = Handlebars.compile(cartTemplate);
     var readyHTML = template(product);
     document.getElementById('cartTable').innerHTML += readyHTML;
+    cardAnimation();
+}
+var removeFromOrder = function(){
+    document.querySelectorAll('.removeFromOrder').forEach(function(element){
+        element.addEventListener('click', function(){
+            var id = element.className.slice(-1);
+            var row = 'rowOfCartTable' + id;
+            var product = productsDB.getElementById(parseInt(id));
+            var row = document.getElementById(row)
+            totalPrice -= parseFloat(product.price);                 
+            row.parentNode.removeChild(row)
+            document.getElementById('totalPrice').innerHTML = parseFloat(totalPrice).toFixed(2);
+            var index = addedProducts.findIndex(x => x.id == id);
+            addedProducts.splice(index, 1);        
+        })
+    })
+}
+var calculatePrice = function(){
+
 }
 
-if(signedUser){
+if (signedUser) {
     signedUser.showAddresses(document.getElementById("availableAddressesInCart"));
-}else{
+} else {
     var p = document.createElement("p")
     p.textContent = "Не сте влезли в профила си. Ако искате да продължите като гост трябва да добавите адрес и телефон за връзка в полето по долу."
     document.getElementById("availableAddressesInCart").appendChild(p);
 }
-cart.addEventListener('click', function(event){
+cart.addEventListener('click', function (event) {
     cartContainer.style.display = 'inline-block';
     background.style.width = '100%';
     background.style.height = '100%';
